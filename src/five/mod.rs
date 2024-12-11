ef69daf9c34a0491e78738dc4bf2eb3f8d011022
Luke Dunscombe <dunscombe1@gmail.com>
2024-12-11 10:32:53 -0600

    WIP trying to brute force, no dice

--- src/five/mod.rs
+++ src/five/mod.rs
@@ -1,18 +1,20 @@
+use itertools::Itertools;
+
 pub fn run() {
     let input = include_str!("input.txt");
-    let part1 = process(input, false);
-    println!("Part1: {}", part1.to_string());
+    // let part1 = process(input, false);
+    // println!("Part1: {}", part1.to_string());
     let part2 = process(input, true);
     println!("Part2: {}", part2.to_string());
 }
 
 fn get_mid(rules: &Vec<Vec<usize>>, update: &Vec<usize>) -> usize {
-    for i in 0..update.len() -1 {
+    for i in 0..update.len() - 1 {
         let vec = &rules[update[i]];
         let page_num = &update[i + 1];
 
         if !vec.contains(page_num) {
-            return 0
+            return 0;
         }
     }
 
@@ -25,18 +27,43 @@ fn process(input: &str, _part2: bool) -> usize {
     let mut rules = vec![Vec::new(); 100];
 
     r.lines().into_iter().for_each(|rule| {
-       let (k, v) = rule.split_once('|').unwrap();
+        let (k, v) = rule.split_once('|').unwrap();
         let key = k.parse::<usize>().unwrap();
         let value = v.parse::<usize>().unwrap();
         rules[key].push(value);
     });
 
     let mut sum = 0;
-    u.lines().into_iter().for_each(|x| {
-        let updates = x.split(",").map(|x| x.parse::<usize>().unwrap()).collect::<Vec<_>>();
 
-        let mid = get_mid(&rules, &updates);
-        sum += mid;
+    let mut line = 0;
+
+    u.lines().into_iter().for_each(|x| {
+        let updates = x
+            .split(",")
+            .map(|x| x.parse::<usize>().unwrap())
+            .collect::<Vec<_>>();
+
+        if _part2 {
+            if get_mid(&rules, &updates) == 0 {
+                let mut n = 0;
+                let perms = updates.iter().cloned().permutations(updates.len());
+                let perm_len = perms.try_len().unwrap();
+                for perm in perms {
+                    n += 1;
+                    let mid = get_mid(&rules, &perm);
+
+                    println!("Line: {} --- permutation: {} / {} --- {}", line, n, perm_len, mid);
+                    if mid != 0 {
+                        sum += mid;
+                        break;
+                    }
+                }
+            }
+            line += 1;
+        } else {
+            let mid = get_mid(&rules, &updates);
+            sum += mid;
+        }
     });
 
     sum
@@ -109,6 +136,6 @@ mod test {
 75,97,47,61,53
 61,13,29
 97,13,75,29,47";
-        assert_eq!(9, process(input, true));
+        assert_eq!(123 , process(input, true));
     }
 }
